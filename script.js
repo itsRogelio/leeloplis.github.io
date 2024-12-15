9e4dd94b487659076250890e627cbc95de23206c
itsRogelio <rogerpapasito777@gmail.com>
2024-12-15 02:50:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+   document.querySelector('button').addEventListener('click', function() {
+       alert('¡Te extraño mucho, solo quisiera que me hables! 💖');
+   });
+});
